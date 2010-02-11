b56b331e92e93bed91d5efd05f3ebe52dfe443c6
saneyuki <saneyuki.str@gmail.com>
2010-02-12 01:59:24 +0900

    Add "modules/Utils.js". Add the preferences. Add options.xul
    Signed-off-by: saneyuki <saneyuki.str@gmail.com>

---
+++ defaults/preferences/adddtounsortbkm.js
@@ -0,0 +1,3 @@
+pref("extensions.add_d_to_unsortedbookmarks.tab.saveTab", true);
+pref("extensions.add_d_to_unsortedbookmarks.content.savePage", true);
+pref("extensions.add_d_to_unsortedbookmarks.content.saveLink", true);
